6e38dbbdb0425b531710c5ec3b0005b6448b2802
alpgiraykocal <alpgiraykocal@gmail.com>
2026-06-03 16:30:59 +0200

    ok

--- 01-website-ready-to-upload/assets/js/site.js
+++ 01-website-ready-to-upload/assets/js/site.js
@@ -3,7 +3,7 @@ import {
   sortPhotos, storyPhotos, absoluteUrl, root, header, footer,
   homeMain, galleryMain, storiesMain, aboutMain, storyMain, legacyStoryMain,
   websiteLdObject, imageGalleryLdObject, personLdObject, articleLdObject,
-} from "./templates.mjs?v=12c62477d8";
+} from "./templates.mjs?v=aa170b3df5";
 
 const DATA_PATH = window.__DATA_PATH__ || "assets/data/site-content.json";
 // Signal that the runtime module loaded & executed. The inline <head> failsafe
